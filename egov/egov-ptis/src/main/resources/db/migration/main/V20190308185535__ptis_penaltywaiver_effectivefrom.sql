6f68c0113b94c41c80be5e1d56ea036b70a91d6a
spiliti <beenzu7@gmail.com>
2021-12-30 04:33:59 +0200

    corrected config to key_id

--- egov/egov-ptis/src/main/resources/db/migration/main/V20190308185535__ptis_penaltywaiver_effectivefrom.sql
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20190308185535__ptis_penaltywaiver_effectivefrom.sql
@@ -1,7 +1,7 @@
 UPDATE eg_appconfig_values 
     SET effectivefrom = '2019-02-16 00:00:00' 
-    WHERE config = (select id from eg_appconfig where keyname = 'PENALTY_WAIVER_ENABLED');
+    WHERE key_id = (select id from eg_appconfig where keyname = 'PENALTY_WAIVER_ENABLED');
 
 UPDATE eg_appconfig_values 
     SET effectivefrom = '2019-02-16 00:00:00' , value='16/02/2019'
-    WHERE config = (select id from eg_appconfig where keyname = 'PENALTY_WAIVER_CUTOFF_DATE');
+    WHERE key_id = (select id from eg_appconfig where keyname = 'PENALTY_WAIVER_CUTOFF_DATE');
