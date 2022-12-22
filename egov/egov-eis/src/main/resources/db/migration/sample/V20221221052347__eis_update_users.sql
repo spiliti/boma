f3e8955aae53e242c7c83c8eb1768b689ac6e9d6
spiliti <beenzu7@gmail.com>
2022-12-22 05:21:53 +0200

    updated sample data for migration and filestore

---
+++ egov/egov-eis/src/main/resources/db/migration/sample/V20221221052347__eis_update_users.sql
@@ -0,0 +1,86 @@
+---Things to do here or before
+-- Boundary type
+--egeis-employee - update employment and date of retirement - None
+
+update egeis_empoyee set dateofretirement = '2025-12-31'
+--- eg_role replace  
+
+UPDATE eg_role SET name = 'LGA Operator' where id = 4;
+
+--- Funds clear add CDF fund
+---- Bank Accounts
+----update designations
+
+
+-- update eis sample data
+
+
+update eg_user set locale = 'en_ZM',pwdexpirydate = '2023-12-31';
+
+-------------------------update eg_user with local names -----------
+UPDATE  eg_user SET username = 'zenzo', name = 'Jane Zenzo' WHERE id = 3;
+UPDATE  eg_user SET username = 'chabala', name = 'Joseph Chabala' WHERE id = 4;
+UPDATE  eg_user SET username = 'mulenga', name = 'Mushuma Mulenga' WHERE id = 5;
+UPDATE  eg_user SET username = 'mtonga', name = 'Melvin Mtonga' WHERE id = 6;
+UPDATE  eg_user SET username = 'banda', name = 'Charles Banda' WHERE id = 7;
+UPDATE  eg_user SET username = 'chanda', name = 'Mumba Chanda' WHERE id = 8;
+UPDATE  eg_user SET username = 'sakwiya', name = 'Martin Sakwiya' WHERE id = 9;
+UPDATE  eg_user SET username = 'mulenje', name = 'Mulele M. Mulenje' WHERE id = 10;
+UPDATE  eg_user SET username = 'kapandula', name = 'Margret C. Kapandula' WHERE id = 11;
+UPDATE  eg_user SET username = 'mulima', name = 'Maketo Mulima' WHERE id = 12;
+UPDATE  eg_user SET username = 'muleya', name = 'Dustford Muleya' WHERE id = 13;
+UPDATE  eg_user SET username = 'muzumi', name = 'Lazarus Muzumi' WHERE id = 14;
+UPDATE  eg_user SET username = 'lungisa', name = 'Richard Lungisa' WHERE id = 15;
+UPDATE  eg_user SET username = 'mudenda', name = 'Saul M. Mudenda' WHERE id = 16;
+UPDATE  eg_user SET username = 'bakale', name = 'Peter Bakale' WHERE id = 17;
+UPDATE  eg_user SET username = 'kaoma', name = 'Sara Kaoma' WHERE id = 18;
+UPDATE  eg_user SET username = 'choseni', name = 'Beth Choseni' WHERE id = 19;
+UPDATE  eg_user SET username = 'masenge', name = 'Sylvia  Masenge' WHERE id = 20;
+UPDATE  eg_user SET username = 'mwinza', name = 'Andrew Mwinza' WHERE id = 21;
+UPDATE  eg_user SET username = 'nswana', name = 'Levi Nswana' WHERE id = 22;
+UPDATE  eg_user SET username = 'mvula', name = 'Cordless Mvula' WHERE id = 23;
+UPDATE  eg_user SET username = 'melody', name = 'Musukuma Melody' WHERE id = 24;
+UPDATE  eg_user SET username = 'phiri', name = 'Maurice Phiri' WHERE id = 25;
+UPDATE  eg_user SET username = 'chileke', name = 'Lameck Chileke' WHERE id = 26;
+UPDATE  eg_user SET username = 'paketi', name = 'Simon Paketi' WHERE id = 27;
+UPDATE  eg_user SET username = 'muzundu', name = 'Bob Muzundu' WHERE id = 28;
+UPDATE  eg_user SET username = 'shawa', name = 'Siza Shawa' WHERE id = 29;
+UPDATE  eg_user SET username = 'bwezani', name = 'Austin Bwezani' WHERE id = 30;
+UPDATE  eg_user SET username = 'nyirongo', name = 'Lawson Nyirongo' WHERE id = 31;
+UPDATE  eg_user SET username = 'nkolomo', name = 'Chewe Nkolomo' WHERE id = 32;
+UPDATE  eg_user SET username = 'chisenga', name = 'Chisenga Chisenga' WHERE id = 33;
+UPDATE  eg_user SET username = 'musa', name = 'Lookout Musa' WHERE id = 34;
+UPDATE  eg_user SET username = 'yamfwa', name = 'Beatrice Yamfwa' WHERE id = 35;
+UPDATE  eg_user SET username = 'kaunda', name = 'John Kaunda' WHERE id = 36;
+UPDATE  eg_user SET username = 'hankanga', name = 'Mary Hankanga' WHERE id = 37;
+UPDATE  eg_user SET username = 'makowa', name = 'Mazuba B. Makowa' WHERE id = 38;
+UPDATE  eg_user SET username = 'zimba', name = 'Paulsen Zimba' WHERE id = 39;
+UPDATE  eg_user SET username = 'mutale', name = 'Peter Mutale' WHERE id = 40;
+UPDATE  eg_user SET username = 'chongo', name = 'Bupe Chongo' WHERE id = 41;
+UPDATE  eg_user SET username = 'chanza', name = 'Melvin Chanza' WHERE id = 42;
+UPDATE  eg_user SET username = 'kabanda', name = 'Ernest Kabanda' WHERE id = 43;
+UPDATE  eg_user SET username = 'mulembi', name = 'Monica Mulembi' WHERE id = 44;
+UPDATE  eg_user SET username = 'chellah', name = 'Mary Chellah' WHERE id = 45;
+UPDATE  eg_user SET username = 'katongo', name = 'Hildah Katongo' WHERE id = 46;
+UPDATE  eg_user SET username = 'mwamba', name = 'Masenke Mwamba' WHERE id = 47;
+UPDATE  eg_user SET username = 'nyirenda', name = 'Suzyo Nyirenda' WHERE id = 48;
+UPDATE  eg_user SET username = 'chilepula', name = 'Maambo Chilepula' WHERE id = 49;
+UPDATE  eg_user SET username = 'mwiingaza', name = 'Samson Mwiingaza' WHERE id = 50;
+UPDATE  eg_user SET username = 'kasongo', name = 'Vyness Kasongo' WHERE id = 51;
+UPDATE  eg_user SET username = 'chileshe', name = 'Melody Chileshe' WHERE id = 52;
+UPDATE  eg_user SET username = 'chirwa', name = 'Tandiwe Chirwa' WHERE id = 53;
+UPDATE  eg_user SET username = 'mulanfu', name = 'Mwansa Mulanfu' WHERE id = 54;
+UPDATE  eg_user SET username = 'muketa', name = 'Mwape Muketa' WHERE id = 55;
+UPDATE  eg_user SET username = 'bwalya', name = 'Joel Bwalya' WHERE id = 56;
+UPDATE  eg_user SET username = 'habeene', name = 'Namoonga Habeene' WHERE id = 57;
+UPDATE  eg_user SET username = 'katumwa', name = 'Ole Katumwa' WHERE id = 58;
+UPDATE  eg_user SET username = 'makani', name = 'Basil Makani' WHERE id = 59;
+UPDATE  eg_user SET username = 'mubiana', name = 'Lackson Mubiana' WHERE id = 60;
+UPDATE  eg_user SET username = 'moono', name = 'Michelle Moono' WHERE id = 61;
+UPDATE  eg_user SET username = 'rama', name = 'Rama Subrey' WHERE id = 62;
+UPDATE  eg_user SET username = 'chipeta', name = 'Joseph Chipeta' WHERE id = 63;
+UPDATE  eg_user SET username = 'mbasela', name = 'George Mbasela' WHERE id = 64;
+UPDATE  eg_user SET username = 'chinyama', name = 'P. Chinyma' WHERE id = 65;
+UPDATE  eg_user SET name = 'Beenzu Besu' WHERE id = 70;
+UPDATE  eg_user SET username = 'beenzu', name = 'Alfred Beenzu' WHERE id = 71;
+UPDATE  eg_user SET username = 'mafosi', name = 'Wilber Mafosi' WHERE id = 72;
