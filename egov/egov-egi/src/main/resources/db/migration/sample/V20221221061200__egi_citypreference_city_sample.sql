d32e30554d7153450ea7945e6b4d21cc2948aca8
spiliti <beenzu7@gmail.com>
2022-12-23 08:50:11 +0200

    update date - localise

--- egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
+++ egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
@@ -1,3 +1,3 @@
 update eg_city set name ='Mapepe City Council', localname = 'Mapepe';
-insert into eg_filestoremap values (1, 1, 'mapepe_logo.png', 'image', '2022-12-21', '2022-12-22',1,1);
+insert into eg_filestoremap values (1, 1, 'mapepe_logo.png', 'image', now(), now(), 1, 1);
 update egcitypreferences set municipalitylogo = 1;
