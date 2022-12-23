f20dde8821c404f3b97c267b7c4136e3a2e610f4
spiliti <beenzu7@gmail.com>
2022-12-24 01:05:01 +0200

    updated filestore.sql

--- egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
+++ egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
@@ -1,3 +1,3 @@
 update eg_city set name ='Mapepe City Council', localname = 'Mapepe';
-insert into eg_filestoremap values (1, 1, 'mapepe_logo.png', 'image', now(), now(), 1, 1);
+insert into eg_filestoremap (id, filestoreid, filename, contenttype, version, createddate, lastmodifieddate, lastmodifiedby, createdby)  values (1, 1, 'mapepe_logo.png', 'image',  1, now(), now(), 1, 1);
 update egcitypreferences set municipalitylogo = 1;
