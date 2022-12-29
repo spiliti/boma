7d00939419b8b31aab0da6d8eb13f099591bba23
spiliti <beenzu7@gmail.com>
2022-12-29 08:40:12 +0200

    Updated mapepe sample migration

--- egov/egov-egi/src/main/resources/db/migration/sample/V20221226061200__update_egi_citypreference_city_sample.sql
+++ egov/egov-egi/src/main/resources/db/migration/sample/V20221226061200__update_egi_citypreference_city_sample.sql
@@ -1,6 +1,1 @@
-insert into eg_filestoremap (id, filestoreid, filename, contenttype, version, createddate, lastmodifieddate, lastmodifiedby, createdby)  values (1, 1, 'mapepe_logo.png', 'image',  1, now(), now(), 1, 1);
-insert into eg_citypreferences (id, municipalitylogo, createdby,createddate, lastmodifiedby, lastmodifieddate, version, municipalityname) values (1,1,1, now(),1,now(), 0, 'Mapepe City Council');
 update eg_city set name ='Mapepe City Council', preferences = 1, localname = 'Mapepe' where domainurl = 'localahost';
-
-
-
