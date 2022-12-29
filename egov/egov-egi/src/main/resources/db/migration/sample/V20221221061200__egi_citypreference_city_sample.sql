7d26cec303e82800cbd0cb987ebee22b41b11d61
spiliti <beenzu7@gmail.com>
2022-12-29 21:55:05 +0200

    Updated eg_city and preferences

--- egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
+++ egov/egov-egi/src/main/resources/db/migration/sample/V20221221061200__egi_citypreference_city_sample.sql
@@ -4,27 +4,3 @@ update eg_city set name ='Mapepe City Council', preferences = 1, localname = 'Ma
 INSERT INTO eg_city (domainurl, name, localname, id, active, version, createdby, lastmodifiedby, createddate, lastmodifieddate, code, districtcode, districtname, longitude, latitude, preferences) VALUES ('41.63.0.133', 'Mapepe City Council', 'Mapepe', nextval('seq_eg_city'), true, 0, 1, 1, '2010-01-01 00:00:00', '2015-01-01 00:00:00', '0001',  NULL, NULL, NULL, NULL, 1);
 INSERT INTO eg_city (domainurl, name, localname, id, active, version, createdby, lastmodifiedby, createddate, lastmodifieddate, code, districtcode, districtname, longitude, latitude, preferences) VALUES ('mapepe2.zm4a.org', 'Mapepe City Council', 'Mapepe', nextval('seq_eg_city'), true, 0, 1, 1, '2010-01-01 00:00:00', '2015-01-01 00:00:00', '0001',  NULL, NULL, NULL, NULL, 1);
 update eg_citypreferences set recaptchapk = '6LfidggTAAAAANDSoCgfkNdvYm3Ugnl9HC8_68o0', recaptchapub = '6LfidggTAAAAADwfl4uOq1CSLhCkH8OE7QFinbVs';
-
-
-      Column      |            Type             | Modifiers
-------------------+-----------------------------+-----------
- domainurl        | character varying(128)      | not null
- name             | character varying(256)      | not null
- localname        | character varying(256)      |
- id               | bigint                      | not null
- active           | boolean                     |
- version          | bigint                      |
- createdby        | numeric                     |
- lastmodifiedby   | numeric                     |
- createddate      | timestamp without time zone |
- lastmodifieddate | timestamp without time zone |
- code             | character varying(4)        |
- districtcode     | character varying(10)       |
- districtname     | character varying(50)       |
- longitude        | double precision            |
- latitude         | double precision            |
- preferences      | numeric                     |
- regionname       | character varying(50)       |
- grade            | character varying(50)       |
- lgdcode          | character varying(10)       |
- districtlgdcode
